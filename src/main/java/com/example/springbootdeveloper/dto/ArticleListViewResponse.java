0525541757164bec775e372c9213ae18520997ed
jjeongdong <donghun3296@naver.com>
2023-09-28 23:55:53 +0900

    feat : 블로그 글 리스트 화면 추가

---
+++ src/main/java/com/example/springbootdeveloper/dto/ArticleListViewResponse.java
@@ -0,0 +1,23 @@
+package com.example.springbootdeveloper.dto;
+
+
+import com.example.springbootdeveloper.domain.Article;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@NoArgsConstructor
+@Getter
+public class ArticleListViewResponse {
+
+    private Long id;
+    private String title;
+    private String content;
+
+    public ArticleListViewResponse(Article article) {
+        this.id = article.getId();
+        this.title = article.getTitle();
+        this.content = article.getContent();
+    }
+}
