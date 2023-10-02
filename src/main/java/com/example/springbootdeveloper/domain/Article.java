c7c21a53b4d291ecfad9bf5eb27391e2cc7d0097
jjeongdong <donghun3296@naver.com>
2023-10-02 12:52:51 +0900

    fix : reatedAt, updatedAt 생성되지 않는 버그 수정

--- src/main/java/com/example/springbootdeveloper/domain/Article.java
+++ src/main/java/com/example/springbootdeveloper/domain/Article.java
@@ -9,9 +9,11 @@ import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
+@EntityListeners(AuditingEntityListener.class)
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
